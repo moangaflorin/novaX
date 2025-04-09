b88952111ed1ce64d4a0e80b8cab02c850867a52
sharmola <sharmolu@proton.me>
2025-04-09 12:32:27 +0300

    implemented login logic

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from db_config import get_db_connection
 from starlette.requests import Request
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+
+
 conn = get_db_connection()
 
-cursor = conn.cursor
+cursor = conn.cursor()
+
+USER_CREDENTIALS = {'username': 'admin', 'password': 'password123'}
+
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
 
 
 @app.get("/")
@@ -24,5 +46,23 @@ async def home(request: Request):
 async def register(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
+@app.post("/login")
+async def login(request: LoginRequest):
+    
+    username = request.username
+    password = request.password
+
+    print(f"Received login request: {request.username}, {request.password}")
+
+    
+    if username == USER_CREDENTIALS['username'] and password == USER_CREDENTIALS['password']:
+        return {"message": "Login successful!"}
+    else:
+        
+        raise HTTPException(status_code=400, detail="Invalid username or password")
+    
+@app.get("/chat")
+async def chat(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
 
 
--- static/js/login.js
+++ static/js/login.js
@@ -10,3 +10,34 @@ window.onload = () => {
         console.error("Register button not found!");
     }
 };
+
+document.getElementById("loginBtn").addEventListener("click", async function(event) {
+    event.preventDefault(); 
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("http://localhost:8000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            window.location.href = "/chat";  
+        } else {
+            alert(data.detail);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        alert("An error occurred while trying to log in. Please try again.");
+    }
+});
